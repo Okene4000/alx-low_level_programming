0f690128a2dfa3dad9d9371080c07c3bc77057fc
Okene4000 <okeneblessing@gmail.com>
2022-12-08 10:26:19 -0800

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a c program that prints a sentence using puts
+ *
+ * return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+}
