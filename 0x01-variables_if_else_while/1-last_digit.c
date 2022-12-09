772052a1941bdb22fe246a5cea6f4cc28d6648e4
Okene4000 <okeneblessing@gmail.com>
2022-12-09 15:19:42 -0800

    C

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ int main(void)
 	{
 		printf(" %d and is 0\n", num);
 	}
-	else if ((n < 6) && (n != 0))
+	else if ((n < 6) && (n < 0))
 	{
 		printf(" %d and is less than 6 and not 0\n", num);
 	}
