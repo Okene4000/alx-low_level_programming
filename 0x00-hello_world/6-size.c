867909e479ea1a1c6d6878a3fbce8b0185319941
Okene4000 <okeneblessing@gmail.com>
2022-12-08 17:38:41 -0800

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ int main(void)
 	printf("size of a char: %lu byte(s)\n", (unsigned long) sizeof(c));
 	printf("size of an int: %lu byte(s)\n", (unsigned long) sizeof(i));
 	printf("size of a long int: %lu byte(s)\n", (unsigned long) sizeof(I));
-	printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(II));
+	printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(I));
 	printf("size of a float: %lu byte(s)\n", (unsigned long) sizeof(f));
 	return (0);
 }
