1b52ac404b259f29e6d09493988a253f6a8630ee
Okene4000 <okeneblessing@gmail.com>
2022-12-10 08:20:12 -0800

    C

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - print single digit number seperated by commas and space
+ * main - print number seperated by commas and space
  *
  * Return: 0
  */
@@ -11,7 +11,7 @@ int main(void)
 	while (num <= '9')
 	{
 		putchar(num);
-		if (num < '9')
+		if (num <= '9')
 		{
 		putchar(',');
 		putchar(' ');
