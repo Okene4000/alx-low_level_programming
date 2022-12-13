6a6b1c510145096584150a534cdc7cd70a26971e
Okene4000 <okeneblessing@gmail.com>
2022-12-13 05:00:34 -0800

    c function

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print the alphabets ten times
+ *
+ * Return: 0
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int ten;
+	int c;
+
+	for (ten = 0; ten <= 9; ten++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
