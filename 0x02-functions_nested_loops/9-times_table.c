883a7322af5a091c381c8421004121664171252c
Okene4000 <okeneblessing@gmail.com>
2022-12-14 01:42:44 -0800

    c function

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,28 +5,39 @@
  */
 void times_table(void)
 {
-	int num, mult, prod;
+	int a, b, c;
 
-	for (num = 0; num <= 9;)
+	for (a = 0; a <= 9; a++)
 	{
-		_putchar('0');
-		num++;
-
-		for (mult = 0; mult <= 9;)
+		for (b = 0; b <= 9; b++)
 		{
-			_putchar(',');
-			_putchar(' ');
-			mult++;
-
-			prod = num * mult;
-
-			if (prod <= 9)
+			c = a * b;
+			if ((c / 10) == 0)
+				if (b == 0)
+				{
+					_putchar('0');
+				}
+			if (b != 0)
+			{
 				_putchar(' ');
-
+				_putchar((c % 10) + '0');
+			}
+			if (b < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 			else
-				_putchar((prod / 10) + '0');
-			_putchar((prod % 10) + '0');
+			{
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+				if (b < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
