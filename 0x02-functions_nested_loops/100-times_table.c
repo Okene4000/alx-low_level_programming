d29e4f8a4b7e2b86903151cf6942376ade321e8a
Okene4000 <okeneblessing@gmail.com>
2022-12-14 09:26:41 -0800

    c function

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -25,13 +25,13 @@ void print_times_table(int n)
 				if (c >= 100)
 				{
 					_putchar((c / 100) + '0');
-					_putchar((c % 10) + '0');
+					_putchar(((c / 10) % 10) + '0');
 				}
 				else if (c <= 99 && c >= 10)
 				{
 					_putchar((c / 10) + '0');
-					_putchar((c % 10) + '0');
 				}
+				_putchar((c % 10) + '0');
 			}
 			_putchar('\n');
 		}
