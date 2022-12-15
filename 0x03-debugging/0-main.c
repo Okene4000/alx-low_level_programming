75ec82cfaed51d978e370dd36d64cee8c525c112
Okene4000 <okeneblessing@gmail.com>
2022-12-15 01:29:34 -0800

    debug

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - test function for positive or negative
+ * main - test function that prints if integer is positive or negative
  *
  * Return: 0
  */
@@ -8,7 +8,8 @@ int main(void)
 {
 	int i;
 
-	i = 0
-		positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
+	
 	return (0);
 }
