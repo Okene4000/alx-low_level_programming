cc2d3d05935f38b68f2d555183dd08ce03e22c2f
Okene4000 <okeneblessing@gmail.com>
2022-12-15 03:51:35 -0800

    debug

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -5,13 +5,13 @@
  * @month: month in number format
  * @year: year
  * @day: day
- * Return: 0
+ * Return: void
  */
 void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
 	{
-		if (month >= 3 && day >= 60)
+		if (month >= 2 && day >= 60)
 		{
 			day++;
 		}
