20f806ab996e593270b335faf774cab9e26373f9
Okene4000 <okeneblessing@gmail.com>
2022-12-16 13:35:47 -0800

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,10 +10,10 @@ void print_most_numbers(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		if ((i == 2) || (i == 4))
+		if ((i != 2) || (i != 4))
 			continue;
 		else
-			_putchar(i + '0');
+			_putchar(i % 10 + '0');
 	}
 	_putchar('\n');
 }
