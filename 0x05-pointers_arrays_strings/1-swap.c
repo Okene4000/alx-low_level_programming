fd35d6630a9da95852f912b45080e412cd449a99
Okene4000 <okeneblessing@gmail.com>
2022-12-19 05:46:52 -0800

    task 0

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * swap_int - swap the values of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: 0 Always
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *a;
+	int *b;
+
+	printf("a = %d, b = %d\n", a, b);
+
+	void swap_int(int *a , int *b);
+
+	printf("a = %d, b = %d\n", a, b);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This Read me file is a 0x05. C - Pointers, arrays and strings project task.
