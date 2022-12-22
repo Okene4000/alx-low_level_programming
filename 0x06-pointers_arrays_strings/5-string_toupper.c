e231904ba4f7167a9e45fad3926896e250472b5a
Okene4000 <okeneblessing@gmail.com>
2022-12-21 23:39:15 -0800

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *string_toupper - function that changes all lowercase to uppercase
+ * @str: parameter
+ * Return: character
+ */
+
+char *string_toupper(char *str)
+{
+	int index = 0;
+
+	while (str[index++])
+	{
+		if (str[index] >= 'a' && str[index] <= 'z')
+			str[index] -= 32;
+	}
+	return (str);
+}
