998c74d66706ae7d5f1b75d39ae757bc867aeafd
Okene4000 <okeneblessing@gmail.com>
2022-12-22 19:12:03 -0800

    task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,7 +1,19 @@
 #include "main.h"
 
-char *add_strings(char *n1, char *n2, char *r, int r_index);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
+/**
+ * infinite_add - adds two numbers
+ * @n1: the first number to be added
+ * @n2: the second number to be added
+ * @r: the  buffer to store the result
+ * @size_r: the buffer size
+ *
+ * Return: if r can store the sum - a pointer to the result
+ *         if r cannot store the sum - 0
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+}
 
 /**
  * add_strings - adds the number stored in two strings
@@ -47,34 +59,4 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 		return (0);
 
 	return (r + r_index + 1);
-}
-/**
- * infinite_add - adds two numbers
- * @n1: the first number to be added
- * @n2: the second number to be added
- * @r: the  buffer to store the result
- * @size_r: the buffer size
- *
- * Return: if r can store the sum - a pointer to the result
- *         if r cannot store the sum - 0
- */
-
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
-{
-	int index, n1_len = 0, n2_len = 0;
-
-	for (index = 0; *(n1 + index); index++)
-		n1_len++;
-	
-	for (index = 0; *(n2 + index); index++)
-		n2_len++;
-	
-	if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
-		return (0);
-
-	n1 += n1_len - 1;
-	n2 += n2_len - 1;
-	*(r + size_r) = '\0';
-
-	return (add_strings(n1, n2, r, --size_r));
 }
