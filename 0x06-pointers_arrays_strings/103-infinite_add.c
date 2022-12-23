d1fb3884336d906a266d1fc34e984166bb7513c9
Okene4000 <okeneblessing@gmail.com>
2022-12-22 19:00:33 -0800

    task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -18,7 +18,7 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 {
 	int num, tens = 0;
 
-	for (; *n1 && *n2; n1--, n2 --, r_index--)
+	for (; *n1 && *n2; n1--, n2--, r_index--)
 	{
 		num = (*n1 - '0') + (*n2 - '0');
 		num += tens;
@@ -27,13 +27,13 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 	}
 	for (; *n1; n1--, r_index--)
 	{
-		num = (*n1 - '0') + 10;
+		num = (*n1 - '0') + tens;
 		*(r + r_index) = (num % 10) + '0';
 		tens = num / 10;
 	}
 	for (; *n2; n2--, r_index--)
 	{
-		num = (*n2 - '0') + 10;
+		num = (*n2 - '0') + tens;
 		*(r + r_index) = (num % 10) + '0';
 		tens = num / 10;
 	}
