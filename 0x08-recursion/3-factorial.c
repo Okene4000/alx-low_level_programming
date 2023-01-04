13e3d25b89a857e3955f8c579d45d2f3331bd8fa
Okene4000 <okeneblessing@gmail.com>
2023-01-04 15:45:22 -0800

    compare string

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * factorial - function that returns the factorial of a given number
- * @n: point block of memory to fill
+ * @n: number
  * @Return: return -1 if n is lower than zero
  *                 0 if n is equal to zero
  *                 factorial
@@ -10,7 +10,7 @@
 int factorial(int n)
 {
 	if (n == 0)
-		return (1);
+		return (0);
 	else if (n < 0)
 		return (-1);
 	else
