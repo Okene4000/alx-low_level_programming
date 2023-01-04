1f546bc39484cd4ef2e4940d133008d9626627ba
Okene4000 <okeneblessing@gmail.com>
2023-01-04 13:35:35 -0800

    put recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,15 +2,16 @@
 /**
  * _puts_recursion - function that prints a string
  * @s: string
+ *Return: void
  */
 
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+		_putchar('\n');
+		return;
 	}
-	_putchar('\n');
-	return;
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
