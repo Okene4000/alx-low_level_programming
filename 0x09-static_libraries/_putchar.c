baac7272d836714f06b91918478565a84f49f502
Okene4000 <okeneblessing@gmail.com>
2023-01-07 04:45:38 -0800

    static

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,20 +1,12 @@
 #include "main.h"
+#include <unistd.h>
 /**
- *main - entry point
- *Return: 0
+ * _putchar - putchar
+ * @c: character
+ * Return: void
  */
 
-int main(void)
+int _putchar(char c)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
+	return (write(1, &c, 1));
 }
