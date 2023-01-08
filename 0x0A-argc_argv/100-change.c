23ac5387efc7eb8ea59dcd242008bb9efd85efa0
Okene4000 <okeneblessing@gmail.com>
2023-01-07 16:39:15 -0800

    Arg

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,21 +11,31 @@
 
 int main(int argc, char *argv[])
 {
-	int position, total, change, aux;
+	int num, j, result;
 	int coins[] = {25, 10, 5, 2, 1};
-
-	position = total = change = aux = 0;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	total = atoi(argv[1]);
+	num = atoi(argv[1]);
+	result = 0;
 
-	if (total <= 0)
+	if (num < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
+
+	for (j = 0; j < 5 && num >= 0; j++)
+	{
+		while (num >= coins[j])
+		{
+			result++;
+			num -= coins[j];
+		}
+	}
+	printf("%d\n", result);
+	return (0);
 }
